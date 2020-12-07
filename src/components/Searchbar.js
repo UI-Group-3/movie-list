6f0cc93fdd4a4804c6bdde150ca3866cd3ef3cda
Mao Zhang <mao.zhang233@gmail.com>
2020-12-06 20:42:08 -0600

    fix issue of sidebar

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,19 +1,17 @@
 import React from "react";
 import { useMediaQuery } from "react-responsive";
 import { Search, SearchText, SearchButton, SearchInfo } from "../style/header";
+import { useSelector } from "react-redux";
+import { selectHeader } from "../redux/selectors";
 
 function Searchbar() {
-  const bIsDesktop = useMediaQuery({
-    query: "(min-width: 1224px)",
-  });
+  const bIsDesktop = useSelector(selectHeader);
 
   return (
     <Search className={bIsDesktop ? "top" : "side"}>
       <SearchText />
       <i className="iconfont zoom">&#xe717;</i>
-      <SearchInfo >
-
-      </SearchInfo>
+      <SearchInfo></SearchInfo>
     </Search>
   );
 }
--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,107 +1,115 @@
 import * as actionTypes from "./actionConstants";
 
 export const clickLiked = (id) => ({
-    type: actionTypes.CLICK_LIKED,
-    payload: {
-        id
-    }
-})
+  type: actionTypes.CLICK_LIKED,
+  payload: {
+    id,
+  },
+});
 
 export const getMovieList = (data) => ({
-    type: actionTypes.GET_MOVIE_LISTS,
-    payload: {
-        data,
-    },
+  type: actionTypes.GET_MOVIE_LISTS,
+  payload: {
+    data,
+  },
 });
 
-export const getSidebarStatus = (val) => ({
-    type: actionTypes.GET_SIDEBAR_VALUE,
-    payload: {
-        val,
-    },
+export const setSidebarStatus = (sidebar_status, platform_style) => ({
+  type: actionTypes.SET_SIDEBAR_STATUS,
+  payload: {
+    sidebar_status,
+    platform_style,
+  },
 });
+
+export const setPlatformStyle = (bIsDesktop) => ({
+  type: actionTypes.SET_PLATFORM_STYLE,
+  payload: {
+    bIsDesktop,
+  },
+});
+
 export const clickSort = (id) => ({
-    type: actionTypes.CLICK_SORT,
-    id,
+  type: actionTypes.CLICK_SORT,
+  id,
 });
 
 export const prevPage = (page) => ({
-    type: actionTypes.PREV_PAGE,
-    payload: {
-        page,
-    },
+  type: actionTypes.PREV_PAGE,
+  payload: {
+    page,
+  },
 });
 export const nextPage = (page) => ({
-    type: actionTypes.NEXT_PAGE,
-    payload: {
-        page,
-    },
+  type: actionTypes.NEXT_PAGE,
+  payload: {
+    page,
+  },
 });
 
 export const getMovieGenres = (data) => {
-    return {
-        type: actionTypes.GET_MOVIE_GENRES,
-        payload: {
-            data,
-        },
-    };
+  return {
+    type: actionTypes.GET_MOVIE_GENRES,
+    payload: {
+      data,
+    },
+  };
 };
 
 export const setClickValue = (id) => {
-    return {
-        type: actionTypes.SET_CLICK_VALUE,
-        payload: {
-            id,
-        },
-    };
+  return {
+    type: actionTypes.SET_CLICK_VALUE,
+    payload: {
+      id,
+    },
+  };
 };
 
 export const setBlockValue = (id) => {
-    return {
-        type: actionTypes.SET_BLOCK_VALUE,
-        payload: { id }
-    };
+  return {
+    type: actionTypes.SET_BLOCK_VALUE,
+    payload: { id },
+  };
 };
 
 export const setItemDetailValue = (id) => {
-    return {
-        type: actionTypes.SET_ITEM_DETAIL_VALUE,
-        payload: {
-            id
-        }
-    };
+  return {
+    type: actionTypes.SET_ITEM_DETAIL_VALUE,
+    payload: {
+      id,
+    },
+  };
 };
 
-
 export const addLikeList = () => {
-    return {
-        type: actionTypes.ADD_LIKE_LIST
-    }
-}
+  return {
+    type: actionTypes.ADD_LIKE_LIST,
+  };
+};
 
 export const removeLikeList = (id) => {
-    return {
-        type: actionTypes.REMOVE_LIKE_LIST,
-        payload: { id }
-    }
-}
+  return {
+    type: actionTypes.REMOVE_LIKE_LIST,
+    payload: { id },
+  };
+};
 
 export const removeBlockList = (id) => {
-    return {
-        type: actionTypes.REMOVE_BLOCK_LIST,
-        payload: { id }
-    }
-}
+  return {
+    type: actionTypes.REMOVE_BLOCK_LIST,
+    payload: { id },
+  };
+};
 
 export const addBlockList = () => {
-    return {
-        type: actionTypes.ADD_BLOCK_LIST
-    }
-}
+  return {
+    type: actionTypes.ADD_BLOCK_LIST,
+  };
+};
 
 export const clickDisliked = (id) => ({
-    type: actionTypes.CLICK_DISLIKED,
-    payload: {
-        id
-    }
-})
+  type: actionTypes.CLICK_DISLIKED,
+  payload: {
+    id,
+  },
+});
